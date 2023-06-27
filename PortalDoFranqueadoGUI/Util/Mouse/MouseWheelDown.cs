d139bd56a8c731b35ec5cbf72236da93a6a3ebe9
Régis Antunes <65835931+regismantunes@users.noreply.github.com>
2023-06-27 17:10:14 -0400

    Improvements on image view

---
+++ PortalDoFranqueadoGUI/Util/Mouse/MouseWheelDown.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Input;
+
+namespace PortalDoFranqueado.Util.Mouse
+{
+    public class MouseWheelDown : MouseGesture
+    {
+        public MouseWheelDown()
+            : base(MouseAction.WheelClick)
+        { }
+
+        public MouseWheelDown(ModifierKeys modifiers)
+            : base(MouseAction.WheelClick, modifiers)
+        { }
+
+        public override bool Matches(object targetElement, InputEventArgs inputEventArgs)
+        {
+            if (!base.Matches(targetElement, inputEventArgs))
+                return false;
+
+            return inputEventArgs is MouseWheelEventArgs args &&
+                args.Delta < 0;
+        }
+    }
+}
